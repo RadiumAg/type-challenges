fa1c09057eda1e8ae7ebc557269c5e124441ae61
Radium <2043869032@qq.com>
2023-08-11 23:24:38 +0800

    chore: commit

--- questions/00005-extreme-readonly-keys/template.ts
+++ questions/00005-extreme-readonly-keys/template.ts
@@ -1,1 +1,11 @@
-type GetReadonlyKeys<T> = any
+import type { Equal } from '@type-challenges/utils'
+
+declare global {
+ type GetReadonlyKeys<T> = {
+   [K in keyof Required<T>]: Equal<{ [F in K]: T[F] }, { -readonly [F in K]: T[F] }> extends true ? never : K
+ }[keyof T]
+}
+
+type c = Equal<{ a?: string }, { a?: string }>
+type d = { [key in keyof { a?: string }]: key }
+
