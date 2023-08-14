e99782e3a54dd9454417d6191e0cb476b566de90
Radium <2043869032@qq.com>
2023-08-14 16:27:56 +0800

    chore: commit

--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,1 +1,19 @@
-declare function SimpleVue(options: any): any
+declare function SimpleVue<
+  D extends () => any,
+  M extends Record<string, any>,
+  C extends Record<string, any>,
+>(options: Options<D, M, C>): any
+
+type Options<
+  D extends () => any,
+  M extends Record<string, any>,
+  C extends Record<string, any>,
+> = {
+  data: D & ThisType<ReturnType<D> & Computed<C> & M>
+  computed: C & ThisType<ReturnType<D> & Computed<C> & M>
+  methods: M & ThisType<ReturnType<D> & Computed<C> & M>
+}
+
+type Computed<C extends Record<string, any>> = {
+  [key in keyof C]: ReturnType<C[key]>
+}
