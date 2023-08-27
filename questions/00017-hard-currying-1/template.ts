18a444f18585caf23132de026192a4080dd180ed
Radium <2043869032@qq.com>
2023-08-27 19:27:48 +0800

    chore: commit

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,12 @@
-declare function Currying(fn: any): any
+declare function Currying<T extends (...args: any) => any>(fn: T): Return<T>
+
+type Return<T extends (...args: any) => any> = T extends (
+  ...args: [infer R, ...infer O]
+) => infer TR
+  ? (
+      arg: R
+    ) => O extends []
+      ? TR
+      : Return<(...args: O) => (TR)>
+  : never
+
