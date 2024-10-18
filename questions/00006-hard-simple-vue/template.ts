690d09344ac23bd71b72942c453ee5d4b191ce4f
张镭银 <zly01858966@lazada.com>
2024-10-18 14:54:02 +0800

    types: update SimpleVue type definitions

--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,1 +1,5 @@
-declare function SimpleVue(options: any): any
+declare function SimpleVue<O, C extends { [key: string]: () => any }, M>(options: {
+  data: () => O
+  computed: ThisType<O> & C
+  methods: ThisType<O & { [key in keyof C]: ReturnType<C[key]> } & M > & M
+}): M & C & O
